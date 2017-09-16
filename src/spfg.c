c7b36bb83796cf560b398a64d6ff5bc5ac6cf318
Cristiano Cortezia <cristiano.cortezia@gmail.com>
2017-09-16 18:13:41 -0300

    Add json gr import/export functions

--- src/spfg.c
+++ src/spfg.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include "spfg/spfg.h"
 
@@ -1257,3 +1258,208 @@ static azjson_spec_t root_spec[] = {
     {.boundary = true}
 };
 
+static spfg_gr_t json_gr;
+
+spfg_err_t spfg_gr_import_json(char *json_str, uint32_t len, spfg_gr_id_t *out_gr_id) {
+
+    spfg_err_t err;
+    spfg_gr_t *gr;
+
+    if (azjson_import(json_str, len, root_spec, &json_gr) != AZJSON_ERROR_NO) {
+        return SPFG_ERROR_FAIL;
+    }
+
+    if ((err = resolve_global_gr(json_gr.id, &gr)) != SPFG_ERROR_NO) {
+
+        // Existing gr not found, attempt to create one from imported data.
+        if (err == SPFG_ERROR_NOT_FOUND) {
+
+            spfg_gr_id_t gr_id;
+
+            if ((err = spfg_gr_create(&gr_id, json_gr.name.chars)) != SPFG_ERROR_NO) {
+                return err;
+            }
+
+            (void) resolve_global_gr(gr_id, &gr);
+
+            gr->id = json_gr.id;
+        }
+
+        // Unexpected error detected during gr resolution, bail out.
+        else {
+            return err;
+        }
+    }
+
+    // Existin gr found, cleanup target memory.
+    else {
+        memset(gr, 0, sizeof(spfg_gr_t));
+        memcpy(&gr->name, &json_gr.name, sizeof(json_gr.name));
+        gr->id = json_gr.id;
+    }
+
+    memcpy(&gr->dps, &json_gr.dps, sizeof(json_gr.dps));
+    memcpy(&gr->dps, &json_gr.dps, sizeof(json_gr.dps));
+    memcpy(&gr->fns, &json_gr.fns, sizeof(json_gr.fns));
+    memcpy(&gr->ctl, &json_gr.ctl, sizeof(spfg_gr_ctl_t));
+
+    spfg_gr_cnt_t gr_idx;
+
+    if ((err = find_global_gr(gr->id, &gr_idx)) != SPFG_ERROR_NO) {
+        return SPFG_ERROR_INVALID_GR_ID;
+    }
+
+    spfg_grx_t *grx = &global_grxs[gr_idx];
+    memset(grx, 0, sizeof(spfg_grx_t));
+    grx->gr = gr;  // TODO: refactor into gr_reindex function
+
+    spfg_fnx_t *fnx = &grx->fnx[gr->ctl.curr_fn_idx];
+    memset(fnx, 0, sizeof(spfg_fnx_t));
+
+    if ((err = resolve_gr_fn(gr, gr->fns[gr->ctl.curr_fn_idx].id, &fnx->fn)) != SPFG_ERROR_NO) {
+        return SPFG_ERROR_FAIL;
+    }
+
+    if ((err = spfg_fn_reindex(grx, fnx)) != SPFG_ERROR_NO) {
+        return SPFG_ERROR_REINDEX_FN;
+    }
+
+    if (out_gr_id) {
+        *out_gr_id = gr->id;
+    }
+
+    return SPFG_ERROR_NO;
+}
+
+
+
+static spfg_err_t sappend(char *output, size_t output_len, uint32_t *vcnt, uint32_t *rcnt, const char *fmt, ...)
+{
+    int result;
+    va_list ap;
+    va_start(ap, fmt);
+
+    result = vsnprintf(&output[*rcnt], output_len - *rcnt, fmt, ap);
+
+    if (result < 0) {
+        // TODO: properly handle error
+        return SPFG_ERROR_FAIL;
+    }
+
+    *vcnt += result;
+    *rcnt = *vcnt > output_len - 1 ? output_len - 1 : *vcnt;
+
+    va_end (ap);
+    return SPFG_ERROR_NO;
+}
+
+spfg_err_t spfg_gr_export_json(spfg_gr_id_t gr_id, char *output, uint32_t output_len, uint32_t *slen)
+{
+    spfg_gr_t *gr;
+    spfg_err_t err;
+    uint32_t rcnt = 0;
+    uint32_t vcnt = 0;
+
+    if (!output) {
+        return SPFG_ERROR_BAD_PARAM_NULL_POINTER;
+    }
+
+    memset(output, 0, output_len);
+
+    if ((err = resolve_global_gr(gr_id, &gr)) != SPFG_ERROR_NO) {
+        return SPFG_ERROR_INVALID_GR_ID;
+    }
+
+    sappend(output, output_len, &vcnt, &rcnt, "{");
+    sappend(output, output_len, &vcnt, &rcnt, "\"id\": %d, ", gr->id);
+    sappend(output, output_len, &vcnt, &rcnt, "\"name\": \"%s\", ", gr->name.chars);
+
+    sappend(output, output_len, &vcnt, &rcnt, "\"fns\": [");
+    for (uint32_t idx = 0; idx < SPFG_MAX_GRID_FNS; idx++) {
+        if (!gr->fns[idx].name.chars[0]) {
+            continue;
+        }
+        sappend(output, output_len, &vcnt, &rcnt, "{");
+        sappend(output, output_len, &vcnt, &rcnt, "\"id\": %d, ", gr->fns[idx].id);
+        sappend(output, output_len, &vcnt, &rcnt, "\"name\": \"%s\", ", gr->fns[idx].name.chars);
+        sappend(output, output_len, &vcnt, &rcnt, "\"type\": %d, ", gr->fns[idx].type);
+        sappend(output, output_len, &vcnt, &rcnt, "\"phase\": %d, ", gr->fns[idx].phase);
+        sappend(output, output_len, &vcnt, &rcnt, "\"in_dp_ids\": [");
+
+        for (uint32_t _idx = 0; _idx < gr->fns[idx].in_dp_ids_len; _idx++) {
+            sappend(output, output_len, &vcnt, &rcnt, "%d", gr->fns[idx].in_dp_ids[_idx]);
+            if (_idx + 1 < gr->fns[idx].in_dp_ids_len) {
+                sappend(output, output_len, &vcnt, &rcnt, ", ");
+            }
+        }
+        sappend(output, output_len, &vcnt, &rcnt, "], ");
+
+        sappend(output, output_len, &vcnt, &rcnt, "\"out_dp_ids\": [");
+        for (uint32_t _idx = 0; _idx < gr->fns[idx].out_dp_ids_len; _idx++) {
+            sappend(output, output_len, &vcnt, &rcnt, "%d", gr->fns[idx].out_dp_ids[_idx]);
+            if (_idx + 1 < gr->fns[idx].out_dp_ids_len) {
+                sappend(output, output_len, &vcnt, &rcnt, ", ");
+            }
+        }
+        sappend(output, output_len, &vcnt, &rcnt, "]");
+
+        sappend(output, output_len, &vcnt, &rcnt, "}");
+        if (idx < SPFG_MAX_GRID_FNS && gr->fns[idx + 1].name.chars[0]) {
+            sappend(output, output_len, &vcnt, &rcnt, ", ");
+        }
+    }
+
+    sappend(output, output_len, &vcnt, &rcnt, "], ");
+    sappend(output, output_len, &vcnt, &rcnt, "\"dps\": [");
+
+    for (uint32_t idx = 0; idx < SPFG_MAX_GRID_DPS; idx++) {
+        if (!gr->dps[idx].name.chars[0]) {
+            continue;
+        }
+
+        sappend(output, output_len, &vcnt, &rcnt, "{");
+        sappend(output, output_len, &vcnt, &rcnt, "\"id\": %d, ", gr->dps[idx].id);
+        sappend(output, output_len, &vcnt, &rcnt, "\"name\": \"%s\", ", gr->dps[idx].name.chars);
+        sappend(output, output_len, &vcnt, &rcnt, "\"type\": %d, ", gr->dps[idx].type);
+
+        switch (gr->dps[idx].type) {
+            case SPFG_DP_INT: {
+                sappend(output, output_len, &vcnt, &rcnt, "\"value\": %d, ", gr->dps[idx].value.integer);
+                break;
+            }
+            case SPFG_DP_REAL: {
+                sappend(output, output_len, &vcnt, &rcnt, "\"value\": %.5G, ", gr->dps[idx].value.real);
+                break;
+            }
+            case SPFG_DP_BOOL: {
+                sappend(output, output_len, &vcnt, &rcnt, "\"value\": %s, ", gr->dps[idx].value.boolean ? "true" : "false");
+                break;
+            }
+            default: {
+                return SPFG_ERROR_FAIL;
+            }
+        }
+
+        sappend(output, output_len, &vcnt, &rcnt, "\"emitted\": %s", gr->dps[idx].emitted ? "true" : "false");
+        sappend(output, output_len, &vcnt, &rcnt, "}");
+
+        if (idx < SPFG_MAX_GRID_DPS && gr->dps[idx + 1].name.chars[0]) {
+            sappend(output, output_len, &vcnt, &rcnt, ", ");
+        }
+    }
+
+    sappend(output, output_len, &vcnt, &rcnt, "], ");
+
+    sappend(output, output_len, &vcnt, &rcnt, "\"ctl\": {");
+    sappend(output, output_len, &vcnt, &rcnt, "\"curr_phase\": %d, ", gr->ctl.curr_phase);
+    sappend(output, output_len, &vcnt, &rcnt, "\"curr_fn_idx\": %d", gr->ctl.curr_fn_idx);
+    sappend(output, output_len, &vcnt, &rcnt, "}");
+
+    sappend(output, output_len, &vcnt, &rcnt, "}");
+
+    if (slen) {
+        *slen = rcnt;
+    }
+
+    return SPFG_ERROR_NO;
+}
