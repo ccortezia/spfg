76e2d1411551a28c422c7526d7f270a2ed7f39f3
Cristiano Cortezia <cristiano.cortezia@gmail.com>
2017-09-23 19:44:15 -0300

    Modularize spfg json

---
+++ src/spfg_json.h
@@ -0,0 +1,15 @@
+#ifndef __SPFG_JSON_H__
+#define __SPFG_JSON_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+spfg_err_t spfg_gr_import_json(char *json_str, uint32_t len, spfg_gr_id_t *out_gr_id);
+spfg_err_t spfg_gr_export_json(spfg_gr_id_t gr_id, char *output, uint32_t output_len, uint32_t *slen);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __SPFG_JSON_H__
