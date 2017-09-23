e2643ddc894ba600f23392ee9ddd29e134831bf6
Cristiano Cortezia <cristiano.cortezia@gmail.com>
2017-09-23 19:29:41 -0300

    Fix coverage files cleaning

--- src/makefile-cov.mk
+++ src/makefile-cov.mk
@@ -2,7 +2,7 @@ COVERAGE = $(NAME).info
 
 GCNOS = $(SOURCES:%.c=%.gcno)
 GCDAS = $(SOURCES:%.c=%.gcda)
-GCOVS = $(shell find . -name *.gcov)
+GCOVS = $(wildcard *.gcov)
 
 $(COVERAGE): $(GCNOS) $(GCDAS)
 	(lcov -t "Coverage on ${SHARED_LIB}" -o ${COVERAGE} -c -d .)
