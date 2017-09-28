66d70db2b4f3e83227b4ad716c5a27fadaeb9197
Cristiano Cortezia <cristiano.cortezia@gmail.com>
2017-09-27 21:56:54 -0300

    Fix test bin path in makefiles

--- makefile-cov.mk
+++ makefile-cov.mk
@@ -15,6 +15,6 @@ gcov: src/*.gcno src/*.gcda tests/*.gcno tests/*.gcno
 
 htmlcov: src/*.gcno src/*.gcda tests/*.gcno tests/*.gcno
 	(make -C src spfg.info)
-	(make -C tests test.info)
-	(genhtml -o htmlcov src/spfg.info tests/test.info)
+	(make -C tests $(TEST_EXE).info)
+	(genhtml -o htmlcov src/spfg.info tests/$(TEST_EXE).info)
 
