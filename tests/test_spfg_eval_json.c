8c504a3717643fd729066fd47dca5a3b28c7a977
Cristiano Cortezia <cristiano.cortezia@gmail.com>
2018-01-12 09:31:33 -0200

    Review json eval test case

--- tests/test_spfg_eval_json.c
+++ tests/test_spfg_eval_json.c
@@ -20,10 +20,19 @@ TEST(eval_json, test_spfg_run_cycle_from_imported_json_should_eval)
     spfg_boolean_t output = false;
     spfg_boolean_t emitted = false;
 
-    char json_snapshot[] = "{\"id\": 1, \"name\": \"valid name\", \"fns\": [{\"id\": 1, \"name\": \"fn0p0\", \"type\": 2, \"phase\": 0, \"in_dp_ids\": [1, 3], \"out_dp_ids\": [2]}, {\"id\": 2, \"name\": \"fn0p1\", \"type\": 2, \"phase\": 0, \"in_dp_ids\": [2, 4], \"out_dp_ids\": [5]}], \"dps\": [{\"id\": 1, \"name\": \"dp0p0\", \"type\": 3, \"value\": true, \"emitted\": false}, {\"id\": 2, \"name\": \"dp0p1\", \"type\": 3, \"value\": true, \"emitted\": true}, {\"id\": 3, \"name\": \"dp1p0\", \"type\": 3, \"value\": true, \"emitted\": false}, {\"id\": 4, \"name\": \"dp1p1\", \"type\": 3, \"value\": false, \"emitted\": false}, {\"id\": 5, \"name\": \"dp0p2\", \"type\": 3, \"value\": false, \"emitted\": false}], \"ctl\": {\"curr_phase\": 0, \"curr_fn_idx\": 1}}";
+    char json_snapshot[] = "{\"id\": 1, \"name\": \"valid name\", "
+        "\"fns\": [{\"id\": 1, \"name\": \"fn0p0\", \"type\": 2, \"phase\": 0, \"in_dp_ids\": [1, 3], \"out_dp_ids\": [2]}, "
+                  "{\"id\": 2, \"name\": \"fn0p1\", \"type\": 2, \"phase\": 1, \"in_dp_ids\": [2, 4], \"out_dp_ids\": [5]}], "
+        "\"dps\": [{\"id\": 1, \"name\": \"dp0p0\", \"type\": 3, \"value\": true, \"emitted\": false}, "
+                  "{\"id\": 2, \"name\": \"dp0p1\", \"type\": 3, \"value\": true, \"emitted\": true}, "
+                  "{\"id\": 3, \"name\": \"dp1p0\", \"type\": 3, \"value\": true, \"emitted\": false}, "
+                  "{\"id\": 4, \"name\": \"dp1p1\", \"type\": 3, \"value\": false, \"emitted\": false}, "
+                  "{\"id\": 5, \"name\": \"dp0p2\", \"type\": 3, \"value\": false, \"emitted\": false}], "
+        "\"ctl\": {\"curr_phase\": 1, \"curr_fn_idx\": 1}}";
+
     TEST_ASSERT_EQUAL(SPFG_ERROR_NO, spfg_gr_import_json(json_snapshot, sizeof(json_snapshot), NULL));
     TEST_ASSERT_EQUAL(SPFG_ERROR_NO, spfg_dp_set_bool(gr_id, dp1p1_id, true));
-    TEST_ASSERT_EQUAL(SPFG_ERROR_NO, spfg_run_cycle(gr_id, gr_id, NULL, NULL));
+    TEST_ASSERT_EQUAL(SPFG_ERROR_NO, spfg_run_cycle(gr_id, 0, NULL, NULL));
     TEST_ASSERT_EQUAL(SPFG_ERROR_NO, spfg_dp_get_bool(gr_id, dp0p2_id, &output, &emitted));
     TEST_ASSERT_EQUAL(true, output);
     TEST_ASSERT_EQUAL(true, emitted);
