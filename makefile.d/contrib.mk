230d8ce3b764c983c6082a8bbeb0a98cc66c64ff
Cristiano Cortezia <cristiano.cortezia@gmail.com>
2018-03-13 23:58:48 -0300

    Enable pytest verbosity on ci target

--- makefile.d/contrib.mk
+++ makefile.d/contrib.mk
@@ -6,4 +6,4 @@ binding_elm:
 	(cd contrib/binding-elm; elm-test)
 
 binding_py:
-	(cd contrib/binding-py && LD_LIBRARY_PATH=lib/spfg python3 setup.py test)
+	(cd contrib/binding-py && LD_LIBRARY_PATH=lib/spfg python3 setup.py test -a "-v")
